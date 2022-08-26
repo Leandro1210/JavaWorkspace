95449b6221d9cf1a031f58f01013c0afe0aa5f00
Leandro Rodrigues <71800377+Leandro1210@users.noreply.github.com>
2022-08-26 12:19:14 -0300

    Update IMC.java

--- eclipse-workspace/Aula01/src/exercicios/IMC.java
+++ eclipse-workspace/Aula01/src/exercicios/IMC.java
@@ -4,24 +4,24 @@ public class IMC {
 
 	public static void main(String[] args) {
 		
-		double result, alturaM = 1.50, alturaF = 1.50;
+		double result, alturaM = 1.50f, alturaF = 1.50f;
 		System.out.println("-------------------------------");
 		System.out.println("IMC Homens");
 		System.out.println("-------------------------------");
 		do {
-			alturaM = alturaM +0.01;
+			alturaM = alturaM +0.01f;
 			result = ((72*alturaM)-58);
 			System.out.println(result);
-		} while (alturaM<=2.00);
+		} while (alturaM<=2.00f);
 		
 		System.out.println("-------------------------------");
 		System.out.println("IMC Mulheres");
 		System.out.println("-------------------------------");
 		do {
-			alturaF = alturaF +0.01;
-			result = ((62.1*alturaF)-43.7);
+			alturaF = alturaF +0.01f;
+			result = ((62.1f*alturaF)-43.7f);
 			System.out.println(result);
-		} while (alturaF<=2.00);
+		} while (alturaF<=2.00f);
 	}
 
 }
